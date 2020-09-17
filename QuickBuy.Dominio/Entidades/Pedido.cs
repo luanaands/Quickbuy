b63aca0dadddba753f1c28370e9a5bc4015482d4
Luana Santana <luana_and11@hotmail.com>
2020-09-17 09:40:24 -0300

    criacao entidades

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QuickBuy.Dominio.ObjetosDeApoio;
+using System;
 using System.Collections.Generic;
+using System.Security.Principal;
 using System.Text;
 
 namespace QuickBuy.Dominio.Entidades
@@ -9,6 +11,15 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
+
+        public DateTime DataPrevistaEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
         public ICollection<ItemPedido> ItemPedidos { get; set; }
 
     }
---
+++ QuickBuy.Dominio/ObjetosDeApoio/FormaPagamento.cs
@@ -0,0 +1,39 @@
+﻿using QuickBuy.Dominio.Enuns;
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetosDeApoio
+{
+    public class FormaPagamento
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+  
+
+        public bool EhBoleto
+        {
+            get { return Id == (int)EnumFormaPagamento.Boleto; }
+        }
+
+        public bool EhCartaoDeCredito
+        {
+            get { return Id == (int)EnumFormaPagamento.CartaoCredito; }
+        }
+
+        public bool EhDeposito
+        {
+            get { return Id == (int)EnumFormaPagamento.Deposito; }
+        }
+
+
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)EnumFormaPagamento.NaoDefenido; }
+        }
+
+    }
+}
